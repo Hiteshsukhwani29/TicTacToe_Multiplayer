b006805836cd2ce318651b076bf9989359fa7e55
Hiteshsukhwani29 <hiteshsukhwani29@gmail.com>
2021-09-28 23:01:06 +0530

    Handle Button Clicks

--- app/src/main/java/com/hitesh/tictactoe/Home.kt
+++ app/src/main/java/com/hitesh/tictactoe/Home.kt
@@ -2,17 +2,123 @@ package com.hitesh.tictactoe
 
 
 import android.os.Bundle
+import android.util.Log
+import android.util.Range
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import kotlinx.android.synthetic.main.fragment_home.*
 
-class Home : Fragment() {
+class Home : Fragment(), View.OnClickListener {
+
+
+    var player = true;
+    var count = 0;
+
+    var b = Array(3){IntArray(3)}
+
+    lateinit var board : Array<Array<Button>>
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val v = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val button = v.findViewById<Button>(R.id.button)
+        val button1 = v.findViewById<Button>(R.id.button1)
+        val button2 = v.findViewById<Button>(R.id.button2)
+        val button3 = v.findViewById<Button>(R.id.button3)
+        val button4 = v.findViewById<Button>(R.id.button4)
+        val button5 = v.findViewById<Button>(R.id.button5)
+        val button6 = v.findViewById<Button>(R.id.button6)
+        val button7 = v.findViewById<Button>(R.id.button7)
+        val button8 = v.findViewById<Button>(R.id.button8)
+        val reset = v.findViewById<Button>(R.id.rstbtn)
+
+        board = arrayOf(
+                arrayOf(button,button1,button2),
+                arrayOf(button3,button4,button5),
+                arrayOf(button6,button7,button8)
+        )
+
+        for (i in board){
+            for (butto in i){
+                butto.setOnClickListener(this)
+            }
+        }
+
+        IntializeBoardStatus()
+
+        reset.setOnClickListener{
+            player = true
+            count = 0
+            IntializeBoardStatus()
+        }
+
+        return v
+    }
+
+    private fun IntializeBoardStatus() {
+
+
+        for(i in 0..2){
+            for(j in 0..2){
+                b[0][0] = -1
+                board[0][0].isEnabled = true
+                board[0][0].text = ""
+            }
+        }
+
+    }
+
+    override fun onClick(v: View) {
+
+        when(v.id){
+            R.id.button ->{
+                updateValue(row = 0, col =0, Player = player)
+            }
+            R.id.button1 ->{
+                updateValue(row = 0, col =1, Player = player)
+            }
+            R.id.button2 ->{
+                updateValue(row = 0, col =2, Player = player)
+            }
+            R.id.button3 ->{
+                updateValue(row = 1, col =0, Player = player)
+            }
+            R.id.button4 ->{
+                updateValue(row = 1, col =1, Player = player)
+            }
+            R.id.button5 ->{
+                updateValue(row = 1, col =2, Player = player)
+            }
+            R.id.button6 ->{
+                updateValue(row = 2, col =0, Player = player)
+            }
+            R.id.button7 ->{
+                updateValue(row = 2, col =1, Player = player)
+            }
+            R.id.button8 ->{
+                updateValue(row = 2, col =2, Player = player)
+            }
+
+        }
+
+    }
+
+    private fun updateValue(row: Int, col: Int, Player: Boolean) {
+
+        val P = if(player) "X" else "O"
+        val value = if(player) 1 else 0
+        board[row][col].apply {
+            isEnabled = false
+            setText(P)
+        }
+        b[row][col] = value
+
     }
 
 
